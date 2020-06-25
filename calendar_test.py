2fe7c2b23b71274a2742538661cf3fce4481faa7
Ray Dodds <ray@raydodds.com>
2020-06-25 11:58:56 -0400

    Initial Commit

---
+++ calendar_test.py
@@ -0,0 +1,136 @@
+
+
+from dq_calendar import DQCalendar as cal
+import unittest
+
+class Test(unittest.TestCase):
+	
+	def setUp(self):
+		self.curr_date = '05/22'
+		self.curr_num = 144
+		self.day_one = '01/01'
+		self.day_one_num = 2
+		self.heb_date = 'H'
+		self.heb_num = 1
+		self.fion_date = 'F'
+		self.fion_num = 92
+		self.dai_date = 'D'
+		self.dai_num = 183
+		self.bayur_date = 'B'
+		self.bayur_num = 274
+		self.iters = 10
+
+	def tearDown(self):
+		pass
+
+	def add_year(year, date):
+		return f'{year}/{date}'
+
+	def num_year(year, num):
+		return (year)*cal.YEAR_LENGTH+num
+
+	def test_dse_day_one_not_fest(self):
+		self.assertEqual(
+				cal.date_to_dse(Test.add_year(0, self.day_one)),
+				self.day_one_num)
+
+	def test_dse_day_one_reflective(self):
+		for i in range(0, self.iters):
+			with self.subTest(i=i):
+				date = Test.add_year(i, self.day_one)
+				num = Test.num_year(i, self.day_one_num)
+				self.assertEqual(
+						cal.date_to_dse(date),
+						num)
+				self.assertEqual(
+						cal.dse_to_date(num),
+						date)
+
+
+
+	def test_dse_fest_heb_reflective(self):
+		for i in range(0, self.iters):
+			with self.subTest(i=i):
+				date = Test.add_year(i, self.heb_date)
+				num = Test.num_year(i, self.heb_num)
+				self.assertEqual(
+						cal.date_to_dse(date),
+						num)
+				self.assertEqual(
+						cal.dse_to_date(num),
+						date)
+
+
+
+	def test_dse_fest_fion_reflective(self):
+		for i in range(0, self.iters):
+			with self.subTest(i=i):
+				date = Test.add_year(i, self.fion_date)
+				num = Test.num_year(i, self.fion_num)
+				self.assertEqual(
+						cal.date_to_dse(date),
+						num)
+				self.assertEqual(
+						cal.dse_to_date(num),
+						date)
+
+
+
+	def test_dse_fest_dai_reflective(self):
+		for i in range(0, self.iters):
+			with self.subTest(i=i):
+				date = Test.add_year(i, self.dai_date)
+				num = Test.num_year(i, self.dai_num)
+				self.assertEqual(
+						cal.date_to_dse(date),
+						num)
+				self.assertEqual(
+						cal.dse_to_date(num),
+						date)
+
+
+	def test_dse_fest_bayur_reflective(self):
+		for i in range(0, self.iters):
+			with self.subTest(i=i):
+				date = Test.add_year(i, self.bayur_date)
+				num = Test.num_year(i, self.bayur_num)
+				self.assertEqual(
+						cal.date_to_dse(date),
+						num)
+				self.assertEqual(
+						cal.dse_to_date(num),
+						date)
+
+
+	def test_dse_convert_is_refective(self):
+		for i in range(0, self.iters):
+			with self.subTest(i=i):
+				date = Test.add_year(i, self.curr_date)
+				num = Test.num_year(i, self.curr_num)
+				self.assertEqual(
+						cal.date_to_dse(date),
+						num)
+				self.assertEqual(
+						cal.dse_to_date(num),
+						date)
+
+	def test_all_for_year_reflective(self):
+		for day in range(1, 31):
+			for month in range(1, 13):
+				with self.subTest(day=day, month=month):
+					date = Test.add_year(0, f'{str(month).zfill(2)}/{str(day).zfill(2)}')
+					self.assertEqual(
+							cal.dse_to_date(cal.date_to_dse(date)),
+							date)
+
+	def test_nums_for_year_reflective(self):
+		for i in range(1, cal.YEAR_LENGTH+1):
+			with self.subTest(i = i):
+				self.assertEqual(
+						cal.date_to_dse(cal.dse_to_date(i)),
+						i)
+
+
+
+if __name__=='__main__':
+	unittest.main()
---
+++ dq_calendar.py
@@ -0,0 +1,87 @@
+"""	dqcalendar.py
+	a short library for doing math on the dungeon quest calendar
+"""
+
+import re
+
+class DQCalendar:
+	MONTHS = ['Ze', 'Di', 'Jha', 'Ka', 'Pho', 'Keo', 'Bak', 'Ti', 'Gu', 'Ze-Tau', 'Ze-Di', 'Ze-Jha']
+	DAYS = ['Titen', 'Cen', 'Kalmis', 'Wentos', 'Obaous', 'Divu']
+	FESTIVALS = ['Heb', 'Fion', 'Dai', 'Bayur']
+	YEAR_LENGTH = 364
+	MONTH_LENGTH = 30
+	FEST_DAYS = {'H': 1, 'F': 92, 'D': 183, 'B': 274}
+
+	YFMT = r'(?P<year>[\de]+)/((?P<month>\d{2})/(?P<day>[0-3]\d)|(?P<fest>[BDFH]))'
+
+	def date_to_dse(date_string):
+		year = 0
+		month = 0
+		day = 0
+		fest = ''
+
+		dse = 0
+
+		res = re.match(DQCalendar.YFMT, date_string)
+
+		if res:
+			ddict = res.groupdict()
+
+			year = ddict['year']
+			year = int(year[year.find('e')+1:])
+
+			if ddict['month']:
+				month = int(ddict['month'])
+				day = int(ddict['day'])
+
+				dse = year*DQCalendar.YEAR_LENGTH + (month-1)*DQCalendar.MONTH_LENGTH + day + (month-1)//3 +1
+
+			else:
+				fest = ddict['fest']
+
+				dse = year*DQCalendar.YEAR_LENGTH + DQCalendar.FEST_DAYS[fest] 
+
+		else:
+			raise ValueError("Invalid date format")
+		
+		return dse
+
+	def dse_to_date(dse_num):
+		year = (dse_num-1)//DQCalendar.YEAR_LENGTH
+		fest = ''
+
+		rem = dse_num - year*DQCalendar.YEAR_LENGTH
+
+		date_string = ''
+
+		if rem in DQCalendar.FEST_DAYS.values():
+			fest = list(DQCalendar.FEST_DAYS.keys())[list(DQCalendar.FEST_DAYS.values()).index(rem)]
+			
+			date_string = f'{year}/{fest}'
+		else:
+			rem = rem-((rem-1)//(DQCalendar.YEAR_LENGTH//4)+2)
+
+			month = (rem)//DQCalendar.MONTH_LENGTH+1
+
+			day = (rem)%(DQCalendar.MONTH_LENGTH)+1
+
+			date_string = f'{year}/{str(month).zfill(2)}/{str(day).zfill(2)}'
+
+		return date_string
+
+	
+class DQDate:
+	def __init__(self, date):
+		if type(date) == str:
+			self.dse = DQCalendar.date_to_dse(date)
+		elif type(date)  == int:
+			self.dse = date
+		else:
+			raise TypeError("Invalid type for date")
+
+	def days_between(self, other):
+		if(type(other) == DQDate):
+			raise TypeError("Date comparisons require two dates")
+		return abs(self.date-other.date)
+
+
